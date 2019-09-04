ccce13fe3b3e36c071ba46605c5cf15e106c40c0
Roberto <bigassonr@gmail.com>
2019-09-04 18:01:04 +0200

    import added again

--- main_training.py
+++ main_training.py
@@ -1,6 +1,9 @@
 def main():
     print("Starting training for all models...\n")
 
+    from text_training import text_train
+    from mnist_training import mnist_train
+
     print("")
     print("Training finished.")
 
