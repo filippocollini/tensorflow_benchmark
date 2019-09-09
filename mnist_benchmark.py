077957af122e923c3f9a681d3d682e4f9a073190
filippocollini <filippo.collini@mail.polimi.it>
2019-09-09 14:52:24 +0200

    mnist benchmark with csv file writing

--- mnist_benchmark.py
+++ mnist_benchmark.py
@@ -67,6 +67,7 @@ def mnist_bench():
 
     bench_log = "benchmarks/mnist-{}.txt".format(datetime.now())
     file = open(bench_log, "w")
+    file.write("Layers,BatchSize,ConvSize,TotalTime,MeanTime\n")
 
     print("\n------------------------------------ MNIST BENCHMARK ------------------------------------\n")
     for i in range(0, len(num_layers)):
@@ -102,9 +103,9 @@ def mnist_bench():
                 elapsed_times.append(elapsed_time)
                 mean_times.append(mean_time)
 
-                file.write("[Layers: " + str(layers) + ", batch size: " + str(batch_size) + ", conv size: " +
-                           str(conv_size) + "]" + " Total time: " + str(elapsed_time) + ", Mean time: " +
-                           str(mean_time) + ";\n")
+                file.write(str(layers) + "," + str(batch_size) + "," +
+                           str(conv_size) + "," + str(elapsed_time) + "," +
+                           str(mean_time) + "\n")
 
                 """ Print expected category + output """
                 # categories = ["top", "trouser", "pullover", "dress", "coat",
